c65f21a4213e975de61d0771c015de0161f0228d
MemoOrtiz <memodor2001@gmail.com>
2023-12-04 14:58:45 -0600

    Se añadieron los cambios necesarios para insertar las tablas autores, editoriales y libros

--- src/conexion.py
+++ src/conexion.py
@@ -25,22 +25,52 @@ for x in records:
     print(str(isbn) +' '+ titulo)
 
 #Insertar registros en BD
-#insert_autores = '''
-#insert into autores values(2,'MEMO','ORTIZ','AGUILAR')
-#'''
+
+#autores
 nombre_autor = input()
 paterno_autor = input()
 materno_autor = input()
 
 # Obtener el próximo valor de la secuencia para el ID
-cursor.execute("SELECT autoincremento.NEXTVAL FROM dual")
+cursor.execute("SELECT MAX(id_autor) + 1 FROM autores")
 id_autor = cursor.fetchone()[0]
+# Si no hay registros en la tabla, establecer el ID en 1
+if id_autor is None:
+    id_autor = 1
 
 insert_autores = '''
 insert into autores(ID_AUTOR,NOMBRE,PATERNO,MATERNO) VALUES (:id_autor,:nombre_autor,:paterno_autor,:materno_autor)
 '''
-valores = {'id_autor':id_autor,'nombre_autor':nombre_autor,'paterno_autor':paterno_autor,'materno_autor':materno_autor}
-cursor.execute(insert_autores,valores)
+valores_autores = {'id_autor':id_autor,'nombre_autor':nombre_autor,'paterno_autor':paterno_autor,'materno_autor':materno_autor}
+cursor.execute(insert_autores,valores_autores)
+connection.commit()
+
+#Insertar en editoriales
+#Ingresar
+nombre_editorial = input()
+# Obtener el valor siguiente de la secuencia para el ID
+cursor.execute("SELECT MAX(id_editorial) + 1 FROM editoriales")
+id_editorial = cursor.fetchone()[0]
+# Si no hay registros en la tabla, establecer el ID en 1
+if id_editorial is None:
+    id_editorial = 1
+
+insert_editoriales = '''
+insert into editoriales(ID_EDITORIAL, NOMBRE) VALUES (:id_editorial,:nombre_editorial)
+'''
+valores_editoriales = {'id_editorial':id_editorial,'nombre_editorial':nombre_editorial}
+cursor.execute(insert_editoriales,valores_editoriales)
+connection.commit()
+
+#Insertar libro
+isbn = input()
+titulo = input()
+
+insert_libros = '''
+insert into libros (ISBN,ID_EDITORIAL,ID_AUTOR,ID_USER,TITULO) VALUES(:isbn,:id_editorial,:id_autor,1,:titulo)
+'''
+valores_libros = {'isbn':isbn,'id_editorial':id_editorial,'id_autor':id_autor,'titulo':titulo}
+cursor.execute(insert_libros,valores_libros)
 connection.commit()
 
 
